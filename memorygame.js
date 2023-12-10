24607cb07d6042cc6b3bced2c736791b6270b1d2
Monika_M <m.marciniak89@gmail.com>
2023-12-10 15:34:32 +0100

    My correct code for flipping cards

--- memorygame.js
+++ memorygame.js
@@ -1,4 +1,29 @@
-let card = document.getElementById('cardId');
-card.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
-  });
+const card = document.createElement('div');
+    card.className = 'card';
+    
+    const image = document.createElement("img");
+   
+    image.src = "doggo.jpg"
+    image.alt = "Card Image Front"
+
+   card.appendChild(image);
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.appendChild(card);
+    
+
+    function flipCard() {
+    var card = document.querySelector('.card');
+    card.classList.toggle("flipped");
+    const imageSrc=document.querySelector("img").src
+    console.log(imageSrc)
+    if(imageSrc.includes('doggo.jpg')){
+      image.src = "./puppy.jpg"
+      image.alt = "Card Image Front"
+    }
+    else{
+      image.src = "./doggo.jpg"
+      image.alt = "Card Image Front"
+    }
+    }
+
+  card.addEventListener("click",flipCard);
